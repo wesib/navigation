e5e096c577b1cb512b604b87aabd5f37a52cd9aa
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-01-27 18:15:22 +0700

    Build using `@run-z/project-config`

--- jest.config.js
+++ jest.config.js
@@ -1,48 +1,7 @@
-export default {
-  preset: 'ts-jest/presets/default-esm',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/spec/**/*.ts',
-    '!src/**/*.spec.ts',
-    '!src/**/index.ts',
-    '!src/**/main.ts',
-    '!**/node_modules/**',
-  ],
-  coverageDirectory: 'target/coverage',
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      functions: 100,
-      lines: 100,
-    },
-  },
-  extensionsToTreatAsEsm: ['.ts'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: '@wesib/navigation',
-        outputDirectory: './target/test-results',
-        classNameTemplate: '{classname}: {title}',
-        titleTemplate: '{classname}: {title}',
-        ancestorSeparator: ' › ',
-        usePathForSuiteName: 'true',
-      },
-    ],
-  ],
+import { configureJest } from '@run-z/project-config';
+
+export default await configureJest({
   restoreMocks: true,
   setupFiles: ['cross-fetch/polyfill'],
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.spec.json',
-        useESM: true,
-      },
-    ],
-  },
-};
+});
